3022888451c9d3f7989187732ef03b8a0ec2889d
Maxim Likhanov <LikhanovMaxim@yandex.ru>
2015-11-22 01:12:15 +0300

    Rename fucnt

--- src/Test/javauniversity/IntArrayUtilsTest.java
+++ src/Test/javauniversity/IntArrayUtilsTest.java
@@ -11,88 +11,84 @@ public class IntArrayUtilsTest {
 
 
     @Test
-    public void EqualDimenAndSizeChangeSize() throws Exception {
+    public void equalDimenAndSizeChangeSize() throws Exception {
         int[] arr = {1, 2, 3, 4, 5};
         assertTrue(Arrays.equals(arr, IntArrayUtils.changeSize(arr, 5)));
     }
 
     @Test
-    public void SizeZeroChangeSize() throws Exception {
+    public void sizeZeroChangeSize() throws Exception {
         int[] arr = {1, 2, 3};
         int[] result = {};
         assertTrue(Arrays.equals(result, IntArrayUtils.changeSize(arr, 0)));
     }
 
     @Test
-    public void EmptyChangeSize() throws Exception {
+    public void emptyChangeSize() throws Exception {
         int[] arr = {};
         int[] resul = {0, 0, 0};
         assertTrue(Arrays.equals(resul, IntArrayUtils.changeSize(arr, 3)));
     }
 
     @Test
-    public void NullChangeSize() throws Exception {
+    public void nullChangeSize() throws Exception {
         int[] arr = {0, 0, 0};
         assertTrue(Arrays.equals(arr, IntArrayUtils.changeSize(null, 3)));
     }
 
     @Test
-    public void SizeMoreDimenChangeSize() throws Exception {
+    public void sizeMoreDimenChangeSize() throws Exception {
         int[] arr = {1, 2, 3, 4, 5};
         int[] result = {1, 2, 3, 4, 5, 0, 0};
         assertTrue(Arrays.equals(result, IntArrayUtils.changeSize(arr, 7)));
     }
 
     @Test
-    public void SizeLessDimenChangeSize() throws Exception {
+    public void sizeLessDimenChangeSize() throws Exception {
         int[] arr = {1, 2, 3, 4, 5};
         int[] result = {1, 2, 3};
         assertTrue(Arrays.equals(result, IntArrayUtils.changeSize(arr, 3)));
     }
 
     @Test
-    public void SizeLessZeroChangeSize() throws Exception {
+    public void sizeLessZeroChangeSize() throws Exception {
         int[] arr = {1, 2, 3, 4, 5};
         assertTrue(Arrays.equals(null, IntArrayUtils.changeSize(arr, -3)));
     }
 
     @Test(timeout = 1000)
-    public void NullCompareArrays() throws Exception {
+    public void nullCompareArrays() throws Exception {
         int[] arr3 = {1, 2, 3};
         assertTrue(IntArrayUtils.compareExcludOrder(null, null));
         assertFalse(IntArrayUtils.compareExcludOrder(null, arr3));
     }
 
     @Test(timeout = 1000)
-    public void EqualarrCompareArrays() throws Exception {
+    public void equallyArrCompareArrays() throws Exception {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {5, 4, 2, 1, 3};
         assertTrue(IntArrayUtils.compareExcludOrder(arr1, arr2));
+        assertTrue(IntArrayUtils.compareExcludOrder(arr2, arr1));
     }
 
     @Test(timeout = 1000)
-    public void EqualDimenCompareArrays() throws Exception {
-        int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {5, 4, 2, 1, 3};
-        assertTrue(IntArrayUtils.compareExcludOrder(arr1, arr2));
-    }
-
-    @Test(timeout = 1000)
-    public void tNoEqualDimenCompareArrays() throws Exception {
+    public void noEqualDimenCompareArrays() throws Exception {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {5, 4, 2};
         assertFalse(IntArrayUtils.compareExcludOrder(arr1, arr2));
+        assertFalse(IntArrayUtils.compareExcludOrder(arr2, arr1));
     }
 
     @Test(timeout = 1000)
-    public void EqualDimenNoarrCompareArrays() throws Exception {
+    public void equalDimenNoEquallyArrCompareArrays() throws Exception {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {5, 4, 2, 2, 2};
         assertFalse(IntArrayUtils.compareExcludOrder(arr1, arr2));
+        assertFalse(IntArrayUtils.compareExcludOrder(arr2, arr1));
     }
 
     @Test(timeout = 1000)
-    public void EmptyCompareArrays() throws Exception {
+    public void emptyCompareArrays() throws Exception {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {};
         assertTrue(IntArrayUtils.compareExcludOrder(arr1, arr1));
@@ -100,13 +96,13 @@ public class IntArrayUtilsTest {
     }
 
     @Test
-    public void shuffleArr() throws Exception {
+    public void testShuffleArr() throws Exception {
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        int[] shufflArr = IntArrayUtils.shuffleArr(arr);
-        assertTrue(IntArrayUtils.compareExcludOrder(arr, shufflArr));
+        int[] shuffleArr = IntArrayUtils.shuffleArr(arr);
+        assertTrue(IntArrayUtils.compareExcludOrder(arr, shuffleArr));
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != shufflArr[i]) {
+            if (arr[i] != shuffleArr[i]) {
                 count++;
             }
         }
@@ -114,7 +110,7 @@ public class IntArrayUtilsTest {
     }
 
     @Test
-    public void NullshuffleArr() throws Exception {
+    public void nullShuffleArr() throws Exception {
         assertTrue(Arrays.equals(null, IntArrayUtils.shuffleArr(null)));
     }
 }
