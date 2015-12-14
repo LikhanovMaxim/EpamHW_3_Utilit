52451757521de33434638c1484a08e5e580b2975
Maxim Likhanov <LikhanovMaxim@yandex.ru>
2015-12-14 18:07:29 +0300

    add test Even

---
+++ src/Test/IntArrayUtils/EvenTest.java
@@ -0,0 +1,34 @@
+package IntArrayUtils;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.TestCase.*;
+
+public class EvenTest {
+
+    @Test
+    public void zeroApply() throws Exception {
+        PredicateIntArr predicate = new Even();
+        int zero = 0;
+        assertTrue(predicate.apply(zero));
+    }
+
+    @Test
+    public void evenApply() throws Exception {
+        PredicateIntArr predicate = new Even();
+        int even = 2;
+        int negativeEven = -24;
+        assertTrue(predicate.apply(even));
+        assertTrue(predicate.apply(negativeEven));
+    }
+
+    @Test
+    public void noEvenApply() throws Exception {
+        PredicateIntArr predicate = new Even();
+        int noEven = 13;
+        int negativeNoEven = -1;
+        assertFalse(predicate.apply(noEven));
+        assertFalse(predicate.apply(negativeNoEven));
+    }
+}
