605b4f03f997740cc250f0e34fafd82418f63777
Maxim Likhanov <LikhanovMaxim@yandex.ru>
2015-12-14 18:07:53 +0300

    add test Prime

---
+++ src/Test/IntArrayUtils/PrimeTest.java
@@ -0,0 +1,41 @@
+package IntArrayUtils;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class PrimeTest {
+
+    @Test
+    public void zeroApply() throws Exception {
+        PredicateIntArr predicate = new Prime();
+        int zero = 0;
+        assertFalse(predicate.apply(zero));
+    }
+
+    @Test
+    public void oneApply() throws Exception {
+        PredicateIntArr predicate = new Prime();
+        int one = 1;
+        assertTrue(predicate.apply(one));
+    }
+
+    @Test
+    public void PrimeApply() throws Exception {
+        PredicateIntArr predicate = new Prime();
+        int prime = 2;
+        int negativePrime = -13;
+        assertTrue(predicate.apply(prime));
+        assertTrue(predicate.apply(negativePrime));
+    }
+
+    @Test
+    public void noPrimeApply() throws Exception {
+        PredicateIntArr predicate = new Prime();
+        int noPrime = 22;
+        int negativeNoPrime = -51;
+        assertFalse(predicate.apply(noPrime));
+        assertFalse(predicate.apply(negativeNoPrime));
+    }
+}
